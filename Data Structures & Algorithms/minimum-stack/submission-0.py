0790167cdf5d742a2c0259d4b8a90b22786ff2c4
Ante Zovko <antezovko.az@gmail.com>
2026-09-08 19:23:26 +0200

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.py
@@ -0,0 +1,18 @@
+class MinStack:
+    def __init__(self):
+        self.container = []
+
+    def push(self, val: int) -> None:
+        self.container.append(val)
+
+    def pop(self) -> None:
+        self.container.pop()
+        
+
+    def top(self) -> int:
+        return self.container[-1]
+        
+
+    def getMin(self) -> int:
+        return min(self.container)
+        
